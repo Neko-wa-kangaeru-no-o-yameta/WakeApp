29298144ff120ac4367a9c83d8d900815aeabccf
willson0v0 <willsonlgz@gmail.com>
2020-10-02 01:17:10 +0800

    Set up basic structure for the app.

---
+++ app/src/main/java/indi/hitszse2020g6/wakeapp/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package indi.hitszse2020g6.wakeapp
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
