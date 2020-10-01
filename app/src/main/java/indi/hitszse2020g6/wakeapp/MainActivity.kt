e790f8261a7a93af492284923beac092de757342
willson0v0 <willsonlgz@gmail.com>
2020-10-01 23:38:27 +0800

    Project Init

---
+++ app/src/main/java/indi/hitszse2020g6/wakeapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package indi.hitszse2020g6.wakeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
