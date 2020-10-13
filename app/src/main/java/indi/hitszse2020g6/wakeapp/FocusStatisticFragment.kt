383c075360666ce60c5def5addd81134dab432fc
Mr-Fatigue <331137797@qq.com>
2020-10-14 06:19:49 +0800

    fixed some bugs, made donutchart can read from database
    # Conflicts:
    #	.idea/jarRepositories.xml
    #	app/build.gradle

--- app/src/main/java/indi/hitszse2020g6/wakeapp/FocusStatisticFragment.kt
+++ app/src/main/java/indi/hitszse2020g6/wakeapp/FocusStatisticFragment.kt
@@ -35,7 +35,8 @@ class FocusStatisticFragment : Fragment() {
         //以下所使用的参数均为全局变量，写入参数列表只是为了明确函数的定义
         showTodayStatistic(focusFrequency, breakTimes, focusTime)
         val lineSet = linkedMapOf<String,Float>()
-        getLineChartDataWeek(myDao,lineSet)
+        val donutPairSet = linkedMapOf<String,Float>()
+        getChartDataWeek(myDao,lineSet,donutPairSet)
         setLineChart(lineSet)
         setDonutChart(donutPairSet, donutColorSet)
     }
@@ -55,7 +56,7 @@ class FocusStatisticFragment : Fragment() {
         var totalTime = 0L
         for(data in statisticList){
             if(data.isCanceled) count++
-            totalTime += data.totalFocusTime
+            else totalTime += data.totalFocusTime
         }
         focusFrequency = statisticList.size//计时的总次数即为总的表项数
         breakTimes = count//计时取消的次数已经计算完成
@@ -75,8 +76,11 @@ class FocusStatisticFragment : Fragment() {
         requireView().findViewById<TextView>(R.id.breakTimes0)!!.apply {
             text = (breakTimes % 10).toString()
         }
+        requireView().findViewById<TextView>(R.id.focusTime3)!!.apply {
+            text = (focusTime / 1000).toString()
+        }
         requireView().findViewById<TextView>(R.id.focusTime2)!!.apply {
-            text = (focusTime / 100).toString()
+            text = (focusTime / 100 % 10).toString()
         }
         requireView().findViewById<TextView>(R.id.focusTime1)!!.apply {
             text = (focusTime / 10 % 10).toString()
@@ -87,34 +91,86 @@ class FocusStatisticFragment : Fragment() {
     }
 
 
-    //lineChart
-    //week statistic
-    private fun getLineChartDataWeek(myDao: RoomDAO,lineSet: LinkedHashMap<String,Float>){
-        val dayTime = (24*60*60000).toLong()
-        val beginDate = Calendar.getInstance().apply{
-            set(Calendar.HOUR_OF_DAY,0)
-            set(Calendar.MINUTE,0)
-            set(Calendar.SECOND,0)
-            set(Calendar.MILLISECOND,0)
-        }.timeInMillis - 6*dayTime
-        val statisticList:List<MyFocusEntry> = myDao.findFocusData(beginDate)
-        //计算计时取消的次数和总共的时长
+
+    //chart week statistic
+    //产生近7天统计数据列表
+    private fun getChartDataWeek(myDao: RoomDAO,lineSet: LinkedHashMap<String,Float>,donutPairSet: LinkedHashMap<String, Float>) {
+        val dayTime = (24 * 60 * 60000).toLong()
+        val beginDate = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }.timeInMillis - 6 * dayTime
+        val statisticList: List<MyFocusEntry> = myDao.findFocusData(beginDate)
+        //计算计时取消的次数和总共的时长,对lineChart使用
         var index = 0
-        for(count in 1..7){
+        for (count in 1..7) {
             var totalTime = 0L
-            while(index < statisticList.size && statisticList[index].focusDate < beginDate + count*dayTime){
-                totalTime += statisticList[index].totalFocusTime
+            while (index < statisticList.size && statisticList[index].focusDate < beginDate + count * dayTime) {
+                if (!statisticList[index].isCanceled) totalTime += statisticList[index].totalFocusTime
                 index++
             }
-            lineSet[String.format("%d",count)] = (totalTime.toFloat()/60)
+            lineSet[String.format("%d", count)] = (totalTime.toFloat() / 60)
+        }
+        //先统计各标题时长，对donutChart使用
+        val donutSet = linkedMapOf<String,Float>()
+        for (data in statisticList) {
+            if (!data.isCanceled) {
+                if (donutSet.containsKey(data.focusTitle)){
+                    val temp = donutSet.remove(data.focusTitle)
+                    if (temp != null) {
+                        donutSet[data.focusTitle] = temp + data.totalFocusTime.toFloat()
+                    }
+                }
+                else donutSet[data.focusTitle] =
+                    data.totalFocusTime.toFloat()
+            }
+        }
+        donutSet.toSortedMap()//转化为降序
+        //饼状图最大显示7个项，如果大于7个则要将后面的数据算作“其他”
+        if(donutSet.size > 7){
+            var counter = 1
+            var otherTotal = 0f
+            for(data in donutSet){
+                if(counter >= 7)
+                {
+                    otherTotal += data.value
+                }
+                else donutPairSet[setLabel(data.key)] = data.value
+                counter++
+            }
+            donutPairSet["其他    "] = otherTotal
+        }
+        else{
+            for(data in donutSet) donutPairSet[setLabel(data.key)] = data.value
         }
     }
 
+    private fun setLabel(label:String):String{
+        var returnLabel = ""
+        if(label.length == 1)
+            returnLabel = "  $label  "
+        else if(label.length == 2)
+            returnLabel = "  $label "
+        else if(label.length == 3)
+            returnLabel = " $label "
+        else if(label.length == 4)
+            returnLabel = " $label"
+        else if(label.length == 5)
+            returnLabel = label
+        else
+            returnLabel = label.substring(0,4)+"..."
+        return returnLabel
+    }
+
+    //lineChart
     private fun setLineChart(Set:LinkedHashMap<String,Float>){
         lineChart.animation.duration = animationDuration
         lineChart.animate(Set)
         lineChart.labelsFormatter = {a:Float->String.format("%-3.1f",a)}
     }
+
     //donutChart
     private fun setDonutTotal(Set:List<Float>): Float {
         var donutTotal = 0f
@@ -123,33 +179,30 @@ class FocusStatisticFragment : Fragment() {
         return donutTotal
     }
 
-    @SuppressLint("CutPasteId")
     private fun setDonutChart(Set: LinkedHashMap<String, Float>,ColorSet:IntArray){
         val legendCreator = requireView().findViewById<LinearLayout>(R.id.legendLayout)
         legendCreator.removeAllViews()
         val donutSet = Set.map { it.value }
         var counter = 0
-
         for (data in Set) {
             val tempLegendData = linkedMapOf(" " to 0f, data.toPair().first to 10f)
             val legendItem = LayoutInflater.from(legendCreator.context)
                 .inflate((R.layout.focus_statistic_legend_item), legendCreator, false)
-            if(counter < Set.size-1)
-                legendItem.findViewById<HorizontalBarChartView>(R.id.horizontalBarChart).barsColor = ColorSet[ColorSet.size - counter-1]
-            else
-                legendItem.findViewById<HorizontalBarChartView>(R.id.horizontalBarChart).barsColor = ColorSet[0]
-            legendItem.findViewById<HorizontalBarChartView>(R.id.horizontalBarChart).animation.duration = legendAnimationDuration
-            legendItem.findViewById<HorizontalBarChartView>(R.id.horizontalBarChart).animate(tempLegendData)
+            val legend = legendItem.findViewById<HorizontalBarChartView>(R.id.horizontalBarChart)
+            legend.barsColor = ColorSet[ColorSet.size - counter-1]
+            legend.animation.duration = legendAnimationDuration
+            legend.animate(tempLegendData)
             legendCreator.addView(legendItem)
             counter++
         }
         val donutChartCreator = requireView().findViewById<LinearLayout>(R.id.donutChartLayout)
         val chartItem = LayoutInflater.from(donutChartCreator.context)
             .inflate((R.layout.focus_statistic_donutchart_item), donutChartCreator, false)
-        chartItem.findViewById<DonutChartView>(R.id.donutChart).animation.duration = animationDuration
-        chartItem.findViewById<DonutChartView>(R.id.donutChart).donutColors = ColorSet
-        chartItem.findViewById<DonutChartView>(R.id.donutChart).donutTotal = setDonutTotal(donutSet)
-        chartItem.findViewById<DonutChartView>(R.id.donutChart).animate(donutSet)
+        val donutChart = chartItem.findViewById<DonutChartView>(R.id.donutChart)
+        donutChart.animation.duration = animationDuration
+        donutChart.donutColors = ColorSet
+        donutChart.donutTotal = setDonutTotal(donutSet)
+        donutChart.animate(donutSet)
         donutChartCreator.addView(chartItem)
     }
 
@@ -176,29 +229,15 @@ class FocusStatisticFragment : Fragment() {
         private var focusFrequency = 0
         private var breakTimes = 0
         private var focusTime = 0
-//        private var lineSet = linkedMapOf(
-//            "label1" to 5f,
-//            "label2" to 4.5f,
-//            "label3" to 4.7f,
-//            "label4" to 3.5f,
-//            "label5" to 3.6f,
-//            "label6" to 7.5f,
-//            "label7" to 7.5f,
-//            "label8" to 10f,
-//            "label9" to 5f,
-//            "label10" to 6.5f,
-//            "label11" to 3f,
-//            "label12" to 4f
+//        private var donutPairSet = linkedMapOf<String, Float>(
+//            "A" to 100f,
+//            "B" to 100f,
+//            "C" to 200f,
+//            "D" to 200f,
+//            "E" to 300f,
+//            "F" to 400f,
+//            "G" to 500f
 //        )
-        private var donutPairSet = linkedMapOf<String, Float>(
-            "A" to 100f,
-            "B" to 100f,
-            "C" to 200f,
-            "D" to 200f,
-            "E" to 300f,
-            "F" to 400f,
-            "G" to 500f
-        )
 
     }
 }
